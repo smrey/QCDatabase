b4fd6d9a58f51e3cfaede2e2c9e5c3435444deda
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2015-10-14 15:57:19 +0100

    Initial addition of tables

--- CreateTables.py
+++ CreateTables.py
@@ -19,23 +19,50 @@ print "Success so far"
 cursor = db.cursor()
 
 #Drop tables prior to creation in here, to avoid conflicts
-table = "Run"
-sqlsyntax = "DROP TABLE IF EXISTS "+table
-#print sqlsyntax
-cursor.execute(sqlsyntax)
+tablist = ["Pipeline","Rds","Chemistry","MiSeqRun"]
+
+for table in tablist[::-1]: #Have to drop tables in the reverse order from where they were created
+    #time.sleep(0.5)
+    sqlsyntax = "DROP TABLE IF EXISTS "+table
+    #print sqlsyntax
+    cursor.execute(sqlsyntax)
 
 #Create tables
 #Where a relationship exists, tables must be created in the order parent and then child
-cursor.execute(""" CREATE TABLE Run (
-        RunID 
-        PatientID BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
-        NHSNumber VARCHAR(10),
-        FirstName VARCHAR(25),
-        LastName VARCHAR(25) NOT NULL,
-        PreviousLastName VARCHAR(25),
-        DateOfBirth DATE,
-        GENDER VARCHAR(1) NOT NULL,
-        Primary key(RunID)
+cursor.execute(""" CREATE TABLE Rds (
+        ReadID BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+        ReadNumber VARCHAR(15) NOT NULL,
+        Indexed TINYINT(1),
+        NumberOfCycles SMALLINT UNSIGNED NOT NULL,
+        Primary key(ReadID)
         )""")
-print "Run table created"
+print "Reads table created"
 
+cursor.execute(""" CREATE TABLE Pipeline (
+        PipelineID SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+        PipelineName VARCHAR(30),
+        PipelineVersion TINYINT(2),
+        Primary key(PipelineID)
+        )""")
+print "Pipeline table created"
+
+cursor.execute(""" CREATE TABLE Chemistry (
+        ChemistryID SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+        ChemistryType VARCHAR(15),
+        Primary key(ChemistryID)
+        )""")
+print "Chemistry table created"
+
+cursor.execute(""" CREATE TABLE MiSeqRun (
+        MiSeqRunID VARCHAR(50) NOT NULL,
+        RunStartDate DATE NOT NULL,
+        FieldProgrammableGateArrayVersion VARCHAR(10) NOT NULL,
+        MiSeqControlSoftwareVersion VARCHAR(10) NOT NULL,
+        RealTimeAnalysisSoftwareVersion VARCHAR(10) NOT NULL,
+        KitVersionNumber TINYINT(2) NOT NULL,
+        ExperimentName VARCHAR(15),
+        Operator VARCHAR(5),
+        PipelineID SMALLINT(5) UNSIGNED NOT NULL,
+        Primary key(MiSeqRunID)
+        )""")
+print "MiSeqRun table created"
